26c1dcec051e0036c18a4d039dcb18fe86c72a7d
paul <78130276+gitpds@users.noreply.github.com>
2022-03-29 15:18:19 -0500

    pds

--- read/script.js
+++ read/script.js
@@ -1,6 +1,35 @@
 'use strict';
 
+const subName = document.querySelector('.submit--name')
+const subEmail = document.querySelector('.submit--email')
+const subBand = document.querySelector('.submit--band')
+const subAge = document.querySelector('.submit--age')
+
+
+
+
+subName.addEventListener('click', function(){
+  console.log('Name');
+});
+
+subEmail.addEventListener('click', function(){
+  console.log('Email');
+});
+
+
+subBand.addEventListener('click', function(){
+  console.log('Band');
+});
+
+subAge.addEventListener('click', function(){
+  console.log('Age');
+});
+
+
+
 const newElement = React.createElement;
+
+
 
 class LikeButton extends React.Component {
   constructor(props) {
