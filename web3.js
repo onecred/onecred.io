98f4f29fe59bae2c68ce5f7897538de3d300adb3
paul <78130276+gitpds@users.noreply.github.com>
2022-03-29 14:21:55 -0500

    pds

--- web3.js
+++ web3.js
@@ -2,8 +2,6 @@ window.ethereum.request({ method: "eth_requestAccounts" });
  
 const web3 = new Web3(window.ethereum);
  
-export default web3;
-
 web3.eth.getBlockNumber(function (error, result) {
     console.log(result)
   })
