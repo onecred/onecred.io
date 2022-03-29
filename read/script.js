9361371866499f5e337e6e6f9e3f136f039c1775
paul <78130276+gitpds@users.noreply.github.com>
2022-03-29 16:06:02 -0500

    pds

--- read/script.js
+++ read/script.js
@@ -1,32 +1,11 @@
 'use strict';
 
-const subFName = document.querySelector('.submit--fname')
-const subLName = document.querySelector('.submit--lname')
-const subEmail = document.querySelector('.submit--email')
-const subBand = document.querySelector('.submit--band')
-const subAge = document.querySelector('.submit--age')
+let getName = function () {
+  let firstNameField = document.getElementById('firstName').value;
+  let lastNameField = document.getElementById('lastName').value;
+  let result = document.getElementById('result');
 
 
+};
 
-
-subFName.addEventListener('click', function(){
-  console.log('First Name');
-});
-
-subLName.addEventListener('click', function(){
-  console.log('Last Name');
-});
-
-subEmail.addEventListener('click', function(){
-  console.log('Email');
-});
-
-
-subBand.addEventListener('click', function(){
-  console.log('Band');
-});
-
-subAge.addEventListener('click', function(){
-  console.log('Age');
-});
-
+console.log(firstNameField, lastNameField, result);
