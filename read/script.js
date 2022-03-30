7edecd0642b998f1c8bd44534948f61695d225c1
paul <78130276+gitpds@users.noreply.github.com>
2022-03-29 20:43:33 -0500

    pds

--- read/script.js
+++ read/script.js
@@ -10,3 +10,16 @@ let getName = function () {
   result.textContent = 'Your name is' +firstNameField + lastNameField;
 };
 
+
+
+window.ethereum.request({ method: "eth_requestAccounts" });
+ 
+const web3 = new Web3(window.ethereum);
+ 
+web3.eth.getBlockNumber(function (error, result) {
+    console.log(result)
+  })
+
+
+web3.eth.getTransaction('0x9fc76417374aa880d4449a1f7f31ec597f00b1f6f3dd2d66f4c9c6c445836d8b§234')
+.then(console.log);
--- read/web3.js
+++ read/web3.js
@@ -1,7 +1,3 @@
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
- 
-web3.eth.getBlockNumber(function (error, result) {
-    console.log(result)
-  })
+
+
+
