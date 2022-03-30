7910e73870c47a92d5ab547e6de2a30307fff450
paul <78130276+gitpds@users.noreply.github.com>
2022-03-29 20:55:53 -0500

    pds

--- read/script.js
+++ read/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-let getName = function () {
-  let firstNameField = document.getElementById('firstName').value;
-  let lastNameField = document.getElementById('lastName').value;
-  let result = document.getElementById('result');
-
-  console.log(firstNameField, lastNameField, result);
-
-  result.textContent = 'Your name is' +firstNameField + lastNameField;
-};
-
 
 
 window.ethereum.request({ method: "eth_requestAccounts" });
